528b7a01c0912278c087a4cc5f31fa2a2ccffcb7
小林響平 <kobayashikyouhei@kobayashikyoutairanoMacBook-Air.local>
2020-10-19 19:33:12 +0900

    課題です

---
+++ php01.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 3;
+echo $b = + 7;
+
+$array_month = [January,February,March,April,May,June,July,August,September,October,November,December];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "あなたの名前";
+$world = "'s World!";
+
+echo $hello . $name . $world;
+
+$tech_boost = "tech";
+$tech_boost = $tech_boost . " boost";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+$2020_calendar = {
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "2月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "Jury" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+};
+
+echo $2020_calendar[December];
+
+?>
+
